7b423c44847720817c8479b1721d835a2bf1e8c3
Kai Chevannes <chevannes.kai@gmail.com>
2025-05-13 21:38:04 +0100

    feat: Queue initial implementation

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,48 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>,
+}
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.length = 0;
+    this.head = undefined;
+    this.tail = undefined;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    this.length += 1;
+    if (this.head === undefined || this.tail === undefined) {
+      this.head = { value: item, next: undefined };
+      this.tail = this.head;
+      return;
     }
 
-    enqueue(item: T): void {
+    const node = { value: item, next: undefined };
+    this.tail.next = node;
+    this.tail = node;
+  }
 
-}
-    deque(): T | undefined {
+  deque(): T | undefined {
+    if (this.head === undefined) {
+      return undefined;
+    }
 
-}
-    peek(): T | undefined {
+    const val = this.head.value;
+    this.head = this.head.next;
+    this.length -= 1;
+    return val;
+  }
 
-}
+  peek(): T | undefined {
+    if (this.head === undefined) {
+      return undefined;
+    }
+
+    return this.head.value;
+  }
 }
