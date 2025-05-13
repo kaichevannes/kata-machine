f51963b9145b8b70cf580e196e6ba5c692070f0f
Kai Chevannes <chevannes.kai@gmail.com>
2025-05-13 18:11:24 +0100

    feat: BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let bottom = 0;
+  let top = haystack.length;
 
+  while (bottom !== top) {
+    let i = bottom + Math.floor((top - bottom) / 2);
+    if (haystack[i] === needle) {
+      return true;
+    } else if (haystack[i] < needle) {
+      bottom = i + 1;
+    } else {
+      top = i;
+    }
+  }
+
+  return false;
 }
