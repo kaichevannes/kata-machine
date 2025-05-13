1c352b1d0b830e516570e7dbe900726156cbc5ab
Kai Chevannes <chevannes.kai@gmail.com>
2025-05-13 19:11:30 +0100

    feat: TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+  let broken = false;
 
+  let i = 0;
+  while (!broken && i < breaks.length) {
+    i += jumpAmount;
+    broken = breaks[i];
+  }
+
+  for (let j = 0; j < i + jumpAmount; j++) {
+    if (breaks[j]) {
+      return j;
+    }
+  }
+
+  return -1;
 }
