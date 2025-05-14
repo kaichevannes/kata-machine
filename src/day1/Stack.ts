67b8a106041a2753ecb620b797ff6234da2bc9c9
Kai Chevannes <chevannes.kai@gmail.com>
2025-05-14 11:45:30 +0100

    feat: Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,43 @@
+type Node<T> = {
+  value: T,
+  prev?: Node<T>,
+}
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  push(item: T): void {
+    const node = { value: item } as Node<T>;
+
+    this.length++;
+    if (!this.head) {
+      this.head = node;
+      return;
     }
 
-    push(item: T): void {
+    node.prev = this.head;
+    this.head = node;
+  }
 
-}
-    pop(): T | undefined {
+  pop(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
 
-}
-    peek(): T | undefined {
+    this.length--;
 
-}
+    const val = this.head.value;
+    this.head = this.head.prev;
+    return val;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
