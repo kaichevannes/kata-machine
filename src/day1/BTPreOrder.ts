2974063c84d3483be29bd30aa58b6e111fbe500d
Kai Chevannes <chevannes.kai@gmail.com>
2025-05-18 16:48:30 +0100

    refactor: switch to using pre recurse post style recursion

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,11 +1,19 @@
-function walk(node: BinaryNode<number> | null): number[] {
-  if (!node) {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+  if (!curr) {
     return []
   }
 
-  return [node.value].concat(walk(node.left)).concat(walk(node.right));
+  // pre
+  path.push(curr.value);
+
+  // recurse
+  walk(curr.left, path);
+  walk(curr.right, path);
+
+  // post
+  return path;
 }
 
 export default function pre_order_search(head: BinaryNode<number>): number[] {
-  return walk(head);
+  return walk(head, []);
 }
