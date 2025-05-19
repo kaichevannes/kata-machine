9b2e1a35a84a702cbb8ba44af679e07d1653e088
Kai Chevannes <chevannes.kai@gmail.com>
2025-05-19 11:31:39 +0100

    feat: BTBFS

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,20 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  let nodes = [head];
 
+  while (nodes.length > 0) {
+    const node = nodes.shift() as BinaryNode<number>;
+
+    if (node.value === needle) {
+      return true;
+    }
+
+    if (node.left) {
+      nodes.unshift(node.left);
+    }
+    if (node.right) {
+      nodes.unshift(node.right);
+    }
+  }
+
+  return false;
 }
